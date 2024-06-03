f520855fb30ef31ac2598c22e792c2ed9f199d0b
Petr Shtuka <simplemailpetr@gmail.com>
2024-06-03 16:47:01 +0300

    fix

--- Sources/Models/MessageModel.swift
+++ Sources/Models/MessageModel.swift
@@ -154,3 +154,59 @@ public struct MessageModel: Decodable, Equatable {
         }
     }
 }
+
+extension MessageModel {
+    func withDeleteUID(_ deleteUID: Bool) -> MessageModel {
+        return MessageModel(
+            id: self.id,
+            authorDisplay: self.authorDisplay,
+            authorID: self.authorID,
+            date: self.date,
+            resID: self.resID,
+            needaction: self.needaction,
+            active: self.active,
+            subject: self.subject,
+            partnerIDs: self.partnerIDs,
+            parentID: self.parentID,
+            body: self.body,
+            recordName: self.recordName,
+            emailFrom: self.emailFrom,
+            displayName: self.displayName,
+            deleteUID: deleteUID,
+            model: self.model,
+            authorAvatar: self.authorAvatar,
+            starred: self.starred,
+            attachmentIDs: self.attachmentIDs,
+            refPartnerIDs: self.refPartnerIDs,
+            subtypeID: self.subtypeID,
+            isAuthorIDBool: self.isAuthorIDBool
+        )
+    }
+    
+    func withActive(_ active: Bool) -> MessageModel {
+        return MessageModel(
+            id: self.id,
+            authorDisplay: self.authorDisplay,
+            authorID: self.authorID,
+            date: self.date,
+            resID: self.resID,
+            needaction: self.needaction,
+            active: active,
+            subject: self.subject,
+            partnerIDs: self.partnerIDs,
+            parentID: self.parentID,
+            body: self.body,
+            recordName: self.recordName,
+            emailFrom: self.emailFrom,
+            displayName: self.displayName,
+            deleteUID: self.deleteUID,
+            model: self.model,
+            authorAvatar: self.authorAvatar,
+            starred: self.starred,
+            attachmentIDs: self.attachmentIDs,
+            refPartnerIDs: self.refPartnerIDs,
+            subtypeID: self.subtypeID,
+            isAuthorIDBool: self.isAuthorIDBool
+        )
+    }
+}
