a89d99294bdee9e1e2a1e2af79932abd62318764
Petr Shtuka <simplemailpetr@gmail.com>
2024-06-08 10:39:31 +0300

    fix

--- Sources/Models/ContactModel.swift
+++ Sources/Models/ContactModel.swift
@@ -8,25 +8,46 @@
 import Foundation
 
 public struct ContactsModel: Codable {
-    var id: Int
-    var street: String?
-    var street2: String?
-    var mobile: String?
-    var phone: String?
-    var zip: String?
-    var city: String?
-    var countryId: [Int: String]?  // Assuming "country_id" is returned as a dictionary with an ID and name
-    var displayName: String?
-    var isCompany: Bool?
-    var parentId: Int?
-    var type: String?
-    var childIds: [Int]?
-    var comment: String?
-    var email: String?
-    var avatar: String?
-    var name: String?
-    var lastUpdate: String?  // "__last_update"
+    public var id: Int
+    public var street: String?
+    public var street2: String?
+    public var mobile: String?
+    public var phone: String?
+    public var zip: String?
+    public var city: String?
+    public var countryId: [Int: String]?  // Assuming "country_id" is returned as a dictionary with an ID and name
+    public var displayName: String?
+    public var isCompany: Bool?
+    public var parentId: Int?
+    public var type: String?
+    public var childIds: [Int]?
+    public var comment: String?
+    public var email: String?
+    public var avatar: String?
+    public var name: String?
+    public var lastUpdate: String?  // "__last_update"
 
+    init(id: Int, street: String? = nil, street2: String? = nil, mobile: String? = nil, phone: String? = nil, zip: String? = nil, city: String? = nil, countryId: [Int : String]? = nil, displayName: String? = nil, isCompany: Bool? = nil, parentId: Int? = nil, type: String? = nil, childIds: [Int]? = nil, comment: String? = nil, email: String? = nil, avatar: String? = nil, name: String? = nil, lastUpdate: String? = nil) {
+        self.id = id
+        self.street = street
+        self.street2 = street2
+        self.mobile = mobile
+        self.phone = phone
+        self.zip = zip
+        self.city = city
+        self.countryId = countryId
+        self.displayName = displayName
+        self.isCompany = isCompany
+        self.parentId = parentId
+        self.type = type
+        self.childIds = childIds
+        self.comment = comment
+        self.email = email
+        self.avatar = avatar
+        self.name = name
+        self.lastUpdate = lastUpdate
+    }
+    
     enum CodingKeys: String, CodingKey {
         case id, street, street2, mobile, phone, zip, city, email, name, comment, type
         case countryId = "country_id"
