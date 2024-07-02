f55c2660dbcf70ed73fc63058acfa471d7f570a7
Petr Shtuka <simplemailpetr@gmail.com>
2024-07-02 18:17:00 +0300

    fix

--- Sources/Services/ContactService/ContactSearchParameters.swift
+++ Sources/Services/ContactService/ContactSearchParameters.swift
@@ -7,25 +7,28 @@
 
 import Foundation
 
+import Foundation
+
 public struct ContactParameters {
-    public var searchName: String = ""
-    public var searchEmail: String = ""
+    public var searchName: String
+    public var searchEmail: String
     public var idFilter: IDFilterType?
     public var uid: Int
-    public var timeZone: String = ""
-    public var language: String = ""
-    public var limit: Int = 100
-    public var serverVersion: Double = 0.0
-    public var lastContactId: Int? = nil
+    public var timeZone: String
+    public var language: String
+    public var limit: Int
+    public var serverVersion: Double
+    public var lastContactId: Int?
     public var customFields: [String]?
     public var additionalParams: [String: Any]?
     public var sessionId: String
-    
-    public init(searchName: String, searchEmail: String, idFilter: IDFilterType? = nil, uid: Int, timeZone: String, language: String, limit: Int, serverVersion: Double, lastContactId: Int? = nil, customFields: [String]? = nil, additionalParams: [String : Any]? = nil, sessionId: String) {
+
+    public init(uid: Int, sessionId: String, searchName: String = "", searchEmail: String = "", idFilter: IDFilterType? = nil, timeZone: String = "", language: String = "", limit: Int = 100, serverVersion: Double = 0.0, lastContactId: Int? = nil, customFields: [String]? = nil, additionalParams: [String: Any]? = nil) {
+        self.uid = uid
+        self.sessionId = sessionId
         self.searchName = searchName
         self.searchEmail = searchEmail
         self.idFilter = idFilter
-        self.uid = uid
         self.timeZone = timeZone
         self.language = language
         self.limit = limit
@@ -33,6 +36,5 @@ public struct ContactParameters {
         self.lastContactId = lastContactId
         self.customFields = customFields
         self.additionalParams = additionalParams
-        self.sessionId = sessionId
     }
 }
