34dda55fa875880320b010a5feb75dcb40abc854
Petr Shtuka <simplemailpetr@gmail.com>
2024-12-03 18:10:53 +0300

    fix

--- Sources/Models/MailChannelMemberModel.swift
+++ Sources/Models/MailChannelMemberModel.swift
@@ -8,17 +8,17 @@
 import Foundation
 
 public struct MailChannelMemberModel: Codable {
-    let channelId: Int?
-    let lastInterestDate: Date?
-    let partnerId: Int?
-    let guestId: Int?
-    let customChannelName: String?
-    let messageUnreadCounter: Int?
-    let fetchedMessageId: Int?
-    let seenMessageId: Int?
-    let lastSeenDate: Date?
-    
-    // Свойственные ключи для маппинга JSON
+    public let channelId: Int?
+    public let partnerId: Int?
+    public let guestId: Int?
+    public let customChannelName: String?
+    public let messageUnreadCounter: Int?
+    public let fetchedMessageId: Int?
+    public let seenMessageId: Int?
+    public let lastInterestDate: Date?
+    public let lastSeenDate: Date?
+
+    // Ключи для маппинга JSON
     enum CodingKeys: String, CodingKey {
         case channelId = "channel_id"
         case lastInterestDate = "last_interest_dt"
@@ -31,14 +31,21 @@ public struct MailChannelMemberModel: Codable {
         case lastSeenDate = "last_seen_dt"
     }
     
-    // Инициализация из Decoder
+    // Инициализация из JSON
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         
+        channelId = try container.decodeIfPresent(Int.self, forKey: .channelId)
+        partnerId = try container.decodeIfPresent(Int.self, forKey: .partnerId)
+        guestId = try container.decodeIfPresent(Int.self, forKey: .guestId)
+        customChannelName = try container.decodeIfPresent(String.self, forKey: .customChannelName)
+        messageUnreadCounter = try container.decodeIfPresent(Int.self, forKey: .messageUnreadCounter)
+        fetchedMessageId = try container.decodeIfPresent(Int.self, forKey: .fetchedMessageId)
+        seenMessageId = try container.decodeIfPresent(Int.self, forKey: .seenMessageId)
+        
+        // Форматирование дат
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-        
-        channelId = try container.decodeIfPresent(Int.self, forKey: .channelId)
         
         if let lastInterestDtString = try container.decodeIfPresent(String.self, forKey: .lastInterestDate) {
             lastInterestDate = dateFormatter.date(from: lastInterestDtString)
@@ -51,17 +58,18 @@ public struct MailChannelMemberModel: Codable {
         } else {
             lastSeenDate = nil
         }
-        
-        partnerId = try container.decodeIfPresent(Int.self, forKey: .partnerId)
-        guestId = try container.decodeIfPresent(Int.self, forKey: .guestId)
-        customChannelName = try container.decodeIfPresent(String.self, forKey: .customChannelName)
-        messageUnreadCounter = try container.decodeIfPresent(Int.self, forKey: .messageUnreadCounter)
-        fetchedMessageId = try container.decodeIfPresent(Int.self, forKey: .fetchedMessageId)
-        seenMessageId = try container.decodeIfPresent(Int.self, forKey: .seenMessageId)
     }
     
-    // Инициализация вручную, если потребуется
-    public init(channelId: Int?, lastInterestDate: Date?, partnerId: Int?, guestId: Int?, customChannelName: String?, messageUnreadCounter: Int?, fetchedMessageId: Int?, seenMessageId: Int?, lastSeenDate: Date?) {
+    // Ручная инициализация
+    public init(channelId: Int?,
+                lastInterestDate: Date?,
+                partnerId: Int?,
+                guestId: Int?,
+                customChannelName: String?,
+                messageUnreadCounter: Int?,
+                fetchedMessageId: Int?,
+                seenMessageId: Int?,
+                lastSeenDate: Date?) {
         self.channelId = channelId
         self.lastInterestDate = lastInterestDate
         self.partnerId = partnerId
