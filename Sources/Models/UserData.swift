da87e957d9d7c40d3b2182ba70c6bc277e2282b6
Petr Shtuka <simplemailpetr@gmail.com>
2025-02-19 07:31:29 +0300

    fix

--- Sources/Models/UserData.swift
+++ Sources/Models/UserData.swift
@@ -12,67 +12,62 @@ public struct ResponseWrapper: Decodable {
 }
 
 public struct UserData: Equatable, Decodable {
-    
+
     public static func == (lhs: UserData, rhs: UserData) -> Bool {
         return lhs.uid == rhs.uid &&
         lhs.name == rhs.name &&
         lhs.partnerID?.id == rhs.partnerID?.id
     }
-    
+
     public var uid: Int?
     public var name: String?
     public var sessionToken: String?
     public var isSuperuser: Bool?
     public var language: String?
     public var timezone: String?
-    public var avatar: String?
     public var partnerID: PartnerID?
     public var serverVersion: Int?
-    
+
+    public var avatar: String {
+        return (serverVersion ?? 0) >= 15 ? "avatar_128" : "image_small"
+    }
+
     private enum CodingKeys: String, CodingKey {
         case uid
+        case id = "id"
         case name
         case sessionToken = "session_id"
         case isSuperuser = "is_superuser"
         case language = "lang"
         case timezone = "tz"
         case partnerID = "partner_id"
+        case serverVersion
     }
-    
-    public init(uid: Int?, name: String?, sessionToken: String?, isSuperuser: Bool?, language: String?, timezone: String?, partnerID: PartnerID?, serverVersion: Int?) {
-        self.uid = uid
-        self.name = name
-        self.sessionToken = sessionToken
-        self.isSuperuser = isSuperuser
-        self.language = language
-        self.timezone = timezone
-        self.partnerID = partnerID
-        self.serverVersion = serverVersion
-        
-        self.avatar = serverVersion ?? 0 >= 15 ? "avatar_128" : "image_small"
-    }
-    
+
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        uid = try container.decodeIfPresent(Int.self, forKey: .uid)
+
+        uid = try container.decodeIfPresent(Int.self, forKey: .uid) ??
+              container.decodeIfPresent(Int.self, forKey: .id)
+
         name = try container.decodeIfPresent(String.self, forKey: .name)
         sessionToken = try container.decodeIfPresent(String.self, forKey: .sessionToken)
         isSuperuser = try container.decodeIfPresent(Bool.self, forKey: .isSuperuser)
         language = try container.decodeIfPresent(String.self, forKey: .language)
         timezone = try container.decodeIfPresent(String.self, forKey: .timezone)
-        
-        if let partnerIDArray = try? container.decodeIfPresent([AnyDecodable].self, forKey: .partnerID), let firstElement = partnerIDArray.first?.value as? Int {
+        serverVersion = try container.decodeIfPresent(Int.self, forKey: .serverVersion)
+
+        if let partnerIDArray = try? container.decodeIfPresent([AnyDecodable].self, forKey: .partnerID),
+           let firstElement = partnerIDArray.first?.value as? Int {
             partnerID = PartnerID(id: firstElement, name: partnerIDArray.dropFirst().first?.value as? String)
         } else if let partnerIDInt = try? container.decodeIfPresent(Int.self, forKey: .partnerID) {
             partnerID = PartnerID(id: partnerIDInt, name: nil)
         } else {
             partnerID = nil
         }
-        
-        self.serverVersion = nil
-        self.avatar = serverVersion ?? 0 >= 15 ? "avatar_128" : "image_small"
     }
 }
+
 
 public struct PartnerID: Decodable {
     public var id: Int?
